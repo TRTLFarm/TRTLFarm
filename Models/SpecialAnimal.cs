ba44dd4314ede008150195feb2a8c42059fed5d1
TRTLFarm <trancemsuiclove@gmail.com>
2019-01-25 08:46:31 +0100

    LeaderBoard model

---
+++ Models/SpecialAnimal.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TRTLFarm.Models
+{
+    public class SpecialAnimal
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; }
+        public string ImagePath { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal ProductionSpeed { get; set; }
+        public int ProductionTimeSeconds { get; set; }
+        public string ProductionUnit { get; set; }
+        public string ProduceImage { get; set; }
+        public string ProduceName { get; set; }
+        /*at what version price/production goes up example 10 means every 10 versions/steals */
+        public int IncrementVersion { get; set; }
+        public int Version { get; set; }
+        public int ProductionPercentageIncrease { get; set; }
+        public int PricePercentageIncrease { get; set; }
+        public int CooldownSeconds { get; set; }
+        public DateTime LastBoughtTime { get; set; }
+        public string OwnerUserId { get; set; }
+        public DateTime ManualLastClaimDate { get; set; }
+    }
+}
---
+++ ViewModels/LeaderBoardModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using TRTLFarm.Models;
+
+namespace TRTLFarm.ViewModels
+{
+    public class LeaderBoardModel
+    {
+        public string CurrentUserPaymentId { get; set; }
+        public int CurrentUserPosition { get; set; }
+        public IEnumerable<LeaderBoard> leaderBoard { get; set; }
+        public List<SpecialAnimal> SpecialAnimals { get; set; }
+
+        [Required]
+        [Display(Name = "Your new nickname")]
+        public string NewNickname { get; set; }
+    }
+
+    public class LeaderBoard
+    {
+        public string PaymentId { get; set; }
+        public decimal SumProdSpeed { get; set; }
+        public string NickName { get; set; }
+        public string UserId { get; set; }
+    }
+}
