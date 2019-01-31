28e9fee22ed992399ee98fe438fef37de75305a0
TRTLFarm <trancemsuiclove@gmail.com>
2019-01-31 15:20:23 +0100

    Add steal method and history

--- Controllers/TheftController.cs
+++ Controllers/TheftController.cs
@@ -93,5 +93,181 @@ namespace TRTLFarm.Controllers
 
             return View(model);
         }
+
+
+        [Authorize]
+        public async Task<IActionResult> Steal(int UnclaimedId)
+        {
+            ApplicationUser user = await _userManager.GetUserAsync(User);
+            /*check cooldown for enemy and yourself*/
+            var theftCooldown = _db.TheftCooldown.Where(tc => tc.UserId == user.Id).FirstOrDefault();
+            if (theftCooldown != null)
+            {
+                int timeFromLastAttack = (int)(DateTime.UtcNow - theftCooldown.LastUserAttack).TotalSeconds - 7200;/*2 hours*/
+                if (timeFromLastAttack < 0)
+                {
+                    TempData["ERROR"] = "You can't attack yet. You still have cooldown of " + Math.Abs(timeFromLastAttack) + " seconds";
+                    return RedirectToAction("Index");
+                }
+            }
+            var unclaimedToAtack = _db.UserAnimals.Where(ua => ua.Id == UnclaimedId).Include(ua => ua.Animal).FirstOrDefault();
+            if (unclaimedToAtack != null)
+            {
+                var userToAttackCooldown = _db.TheftCooldown.Where(tc => tc.UserId == unclaimedToAtack.UserId).FirstOrDefault();
+                if (userToAttackCooldown != null)
+                {
+                    int timeFromLastAttack = (int)(DateTime.UtcNow - userToAttackCooldown.LastBeenAtacked).TotalSeconds - 7200;/*Safe for 60 minutes*/
+                    if (timeFromLastAttack < 0)
+                    {
+                        TempData["ERROR"] = "You can't attack this player yet. His safe zone is still " + Math.Abs(timeFromLastAttack) + " seconds";
+                        return RedirectToAction("Index");
+                    }
+                }
+
+                if (user.Id == unclaimedToAtack.UserId)
+                {
+                    TempData["ERROR"] = "You can't attack yourself lol....";
+                    return RedirectToAction("Index");
+                }
+
+                /*Calculate stealing capacity*/
+                decimal stealPower = decimal.Zero;
+                decimal YourTotalProduction = decimal.Zero;
+                var OwnedAnimals = _db.UserAnimals.Where(ua => ua.UserId == user.Id).Include(ua => ua.Animal).ToList();
+                var OwnedSpecialAnimals = _db.SpecialAnimals.Where(ua => ua.OwnerUserId == user.Id).ToList();
+                foreach (var animal in OwnedAnimals)
+                {
+                    YourTotalProduction += (animal.AnimalCount * animal.Animal.ProductionSpeed);
+                }
+                /*check if you own any special and add it here*/
+                if (OwnedSpecialAnimals != null)
+                {
+                    foreach (var mySpecialAnimal in OwnedSpecialAnimals)
+                    {
+                        YourTotalProduction += mySpecialAnimal.ProductionSpeed;
+                    }
+                }
+                if (YourTotalProduction > 0)
+                {
+                    stealPower = (int)(YourTotalProduction / 24);
+                    if ((int)(stealPower) > 0)
+                    {
+                        /*Calculate unclaimed count from this useranimal*/
+                        DateTime now = DateTime.UtcNow;
+                        double secondsDif = (now - unclaimedToAtack.LastClaimTime).TotalSeconds;
+                        decimal Produce = Convert.ToDecimal((secondsDif / unclaimedToAtack.Animal.ProductionTimeSeconds) * unclaimedToAtack.AnimalCount * decimal.ToDouble(unclaimedToAtack.Animal.ProductionSpeed));
+                        if (Produce > 0)
+                        {
+                            if (stealPower >= Produce)
+                            {
+                                /*get current user's first useranimal*/
+                                var firstUsersAnimal = OwnedAnimals.FirstOrDefault();
+                                if (firstUsersAnimal != null)
+                                {
+                                    firstUsersAnimal.ClaimedProduceCount += (int)Produce; /*add to current user*/
+                                    unclaimedToAtack.LastClaimTime = DateTime.UtcNow;
+                                    _db.UserAnimals.Update(firstUsersAnimal);
+                                    _db.UserAnimals.Update(unclaimedToAtack);
+                                    if (theftCooldown == null)
+                                    {
+                                        _db.TheftCooldown.Add(new TheftCooldown() { UserId = user.Id, LastUserAttack = DateTime.UtcNow, TotalStealCount = (int)Produce, LastBeenAtacked = DateTime.UtcNow.AddYears(-1) });
+                                    }
+                                    else
+                                    {
+                                        theftCooldown.TotalStealCount += (int)Produce;
+                                        theftCooldown.LastUserAttack = DateTime.UtcNow;
+                                        _db.TheftCooldown.Update(theftCooldown);
+                                    }
+                                    if (userToAttackCooldown == null)
+                                    {
+                                        _db.TheftCooldown.Add(new TheftCooldown() { UserId = unclaimedToAtack.UserId, LastUserAttack = DateTime.UtcNow.AddYears(-1), LastBeenAtacked = DateTime.UtcNow });
+                                    }
+                                    else
+                                    {
+                                        userToAttackCooldown.LastBeenAtacked = DateTime.UtcNow;
+                                        _db.TheftCooldown.Update(userToAttackCooldown);
+                                    }
+                                    _db.UserLogHistory.Add(new UserLogHistory()
+                                    {
+                                        LogType = "Message",
+                                        LogMessage = "You stole " + (int)Produce + " production",
+                                        UserId = user.Id,
+                                        Action = "Steal",
+                                        Date = DateTime.UtcNow
+                                    });
+                                    _db.UserLogHistory.Add(new UserLogHistory()
+                                    {
+                                        LogType = "Message",
+                                        LogMessage = "You lost " + (int)Produce + " production",
+                                        UserId = unclaimedToAtack.UserId,
+                                        Action = "Steal",
+                                        Date = DateTime.UtcNow
+                                    });
+                                    _db.SaveChanges();
+                                    TempData["SUCCESS"] = "You stole " + (int)Produce + " production";
+                                    return RedirectToAction("Index");
+                                }
+                            }
+                            else
+                            {
+                                var firstUsersAnimal = OwnedAnimals.FirstOrDefault();
+                                if (firstUsersAnimal != null)
+                                {
+                                    firstUsersAnimal.ClaimedProduceCount += (int)stealPower;
+                                    /*Calculate time to add to lastclaim time based on stealpower*/
+                                    decimal secondsToAdd = decimal.Zero;
+                                    secondsToAdd = ((unclaimedToAtack.Animal.ProductionTimeSeconds / unclaimedToAtack.AnimalCount) / unclaimedToAtack.Animal.ProductionSpeed) * (int)stealPower;
+                                    unclaimedToAtack.LastClaimTime = unclaimedToAtack.LastClaimTime.AddSeconds(Decimal.ToDouble(secondsToAdd));
+                                    _db.UserAnimals.Update(unclaimedToAtack);
+                                    _db.UserAnimals.Update(firstUsersAnimal);
+                                    if (theftCooldown == null)
+                                    {
+                                        _db.TheftCooldown.Add(new TheftCooldown() { UserId = user.Id, LastUserAttack = DateTime.UtcNow, TotalStealCount = (int)stealPower, LastBeenAtacked = DateTime.UtcNow.AddYears(-1) });
+                                    }
+                                    else
+                                    {
+                                        theftCooldown.TotalStealCount += (int)stealPower;
+                                        theftCooldown.LastUserAttack = DateTime.UtcNow;
+                                        _db.TheftCooldown.Update(theftCooldown);
+                                    }
+                                    if (userToAttackCooldown == null)
+                                    {
+                                        _db.TheftCooldown.Add(new TheftCooldown() { UserId = unclaimedToAtack.UserId, LastUserAttack = DateTime.UtcNow.AddYears(-1), LastBeenAtacked = DateTime.UtcNow });
+                                    }
+                                    else
+                                    {
+                                        userToAttackCooldown.LastBeenAtacked = DateTime.UtcNow;
+                                        _db.TheftCooldown.Update(userToAttackCooldown);
+                                    }
+                                    _db.UserLogHistory.Add(new UserLogHistory()
+                                    {
+                                        LogType = "Message",
+                                        LogMessage = "You stole " + (int)stealPower + " production",
+                                        UserId = user.Id,
+                                        Action = "Steal",
+                                        Date = DateTime.UtcNow
+                                    });
+                                    _db.UserLogHistory.Add(new UserLogHistory()
+                                    {
+                                        LogType = "Message",
+                                        LogMessage = "You lost " + (int)stealPower + " production",
+                                        UserId = unclaimedToAtack.UserId,
+                                        Action = "Steal",
+                                        Date = DateTime.UtcNow
+                                    });
+                                    _db.SaveChanges();
+                                    TempData["SUCCESS"] = "You stole " + (int)stealPower + " production";
+                                    return RedirectToAction("Index");
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            return RedirectToAction("Index");
+        }
+
+
     }
 }
---
+++ Models/UserLogHistory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TRTLFarm.Models
+{
+    public class UserLogHistory
+    {
+        public int Id { get; set; }
+        public string LogType { get; set; }
+        public string UserId { get; set; }
+        public string LogMessage { get; set; }
+        public DateTime Date { get; set; }
+        public string Action { get; set; }
+    }
+}
